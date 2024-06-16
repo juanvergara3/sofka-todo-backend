f0fd4190a0a72dd24a4e994dc8915c2db484dcdb
Juan Vergara <juan.e.vergara@hotmail.com>
2024-06-15 21:12:35 -0500

    helmet y CORS
    - Se habilita el uso de helmet para darle a la API protección básica.
    - Se habilita y configura CORS para solo aceptar requests desde el frontend.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { MongooseExceptionFilter } from './filters/mongoose-exception.filter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    methods: 'GET,POST,PATCH,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+
+  });
 
   app.setGlobalPrefix('api/');
 
